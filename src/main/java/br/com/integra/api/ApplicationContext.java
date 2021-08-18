4d4a4ad85f32825bb18b41f13aa7f1c5068c3bc0
Rafael Lopes <galotinhodelta@gmail.com>
2021-08-18 15:16:40 -0300

    Correção

--- src/main/java/br/com/integra/api/ApplicationContext.java
+++ src/main/java/br/com/integra/api/ApplicationContext.java
@@ -2,9 +2,8 @@ package br.com.integra.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+//@EnableEurekaClient
 @SpringBootApplication
 public class ApplicationContext {
 
